8f78beca8ba2bd0e979a0f9d3fbc613f6a56a89e
deevika sree <nireshbe1992@gmail.com1188 git config --global user.email nireshbe1992@gmail.com>
2019-05-10 10:44:19 +0530

    sample program completed

---
+++ src/org/Sample/Login.java
@@ -0,0 +1,21 @@
+package org.Sample;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Login {
+	public static void main(String[] args) {
+		List li=new ArrayList();
+		li.add(10);
+		li.add(20);
+		li.add(30);
+		li.add(40);
+		li.add(50);
+		li.add(10);
+		System.out.println(li);
+		int si=li.size();
+		System.out.println(si);
+		
+	}
+
+}
