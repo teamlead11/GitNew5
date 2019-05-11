92a9c646246768d6103cfd0a12c1a7fd758d8955
manier404 <t.manier404@gmail.com>
2019-05-11 18:58:23 +0530

    Conflict by Manivannan

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -1,39 +1,13 @@
 package testpack;
 
 public class BaseClass {
-	 static void bubbleSortbyManivannan(int[] arr) {
-	      int n = arr.length;
-	      int temp = 0;
-
-	      for(int i = 0; i < n; i++) {
-	         for(int j=1; j < (n-i); j++) {
-	            if(arr[j-1] > arr[j]) {
-	               temp = arr[j-1];
-	               arr[j-1] = arr[j];
-	               arr[j] = temp;
-	            }
-	         }
-	      }
-	   }
+private void method() {
+	// TODO Auto-generated method stub
+System.out.println("Creating Confict");
+}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is my base class");
-		
-		
-		      int arr[] = { 9,8,7,6,5 };
-		      System.out.println("Array Before Bubble Sort");
-
-		      for(int i = 0; i < arr.length; i++) {
-		         System.out.print(arr[i] + " ");
-		      }
-		      System.out.println();
-		      bubbleSortbyManivannan(arr);
-		      System.out.println("Array After Bubble Sort");
-
-		      for(int i = 0; i < arr.length; i++) {
-		         System.out.print(arr[i] + " ");
-		      }
-		   }
 	}
 
-
+}
