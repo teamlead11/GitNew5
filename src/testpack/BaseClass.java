a4196c900eac9df3af85ffbba25f14c8f79473c2
kalaiselvan09 <kalaiselvanrk09@gmail.com>
2019-05-11 19:36:27 +0530

    kalai bubble sort

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -2,9 +2,41 @@ package testpack;
 
 public class BaseClass {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("this is my base class");
+			
+		public static void main(String[] args) {
+			
+			int a[]= {10,2,6,29};
+			
+			int temp=0;
+			 
+		    int len=a.length;
+		    
+			
+			for (int i=0;i<len;i++)
+				{
+				for (int j=1;j<(len-i);j++)
+				{
+					if (a[j-1]>a[j])
+					{
+						temp=a[j-1];
+						a[j-1]=a[j];
+						a[j]=temp;
+			
+					}
+				}
+				}
+			
+		
+				for (int i=0;i<len;i++)
+				{
+					System.out.println(a[i]);
+				}
+		
+				System.out.println("this is Kalaiselvan base class");
+					
+			
 	}
+
+		
 
 }
