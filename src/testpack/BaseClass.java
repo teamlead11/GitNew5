d6abce39530cadc64fbb8a3122af624e68ca868c
keerthana <keerthana792@gmail.com>
2019-05-11 21:22:36 +0530

    added bubblesortKeerthana

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -7,4 +7,25 @@ public class BaseClass {
 		System.out.println("this is my base class");
 	}
 
+	public void bubblesortKeerthana() {
+		int a[]= {7,6,4,9,1};
+		int t;
+		for(int i=0;i<4;i++)
+		{
+			for(int j=0;j<4-i;j++)
+			{
+				if(a[j]>a[j+1])
+				{
+					t=a[j+1];
+					a[j+1]=a[j];
+					a[j]=t;
+				}
+			
+			}
+		}
+		for(int k=0;k<=4;k++)
+		{
+			System.out.println(a[k]);
+		}
+	}
 }
