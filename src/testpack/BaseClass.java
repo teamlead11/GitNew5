3560bb0f07b2ddc0110728ec2661f56cd2fa30a1
sethujack <sethupathi004@gmail.com>
2019-05-12 21:20:12 +0530

    sethu bubblesort

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -218,7 +218,45 @@ for (int i = 0; i <5; i++) {
 	
 }    
 
+====================================================
+package testpackage;
 
+import java.util.Scanner;
+
+public void class bubblesortSethu {
+
+	public static void main(String[] args) {
+		 int n, c, d, swap;
+		    Scanner in = new Scanner(System.in);
+		 
+		    System.out.println("Input number of integers to sort");
+		    n = in.nextInt();
+		 
+		    int array[] = new int[n];
+		 
+		    System.out.println("Enter " + n + " integers");
+		 
+		    for (c = 0; c < n; c++)
+		      array[c] = in.nextInt();
+		   
+		    for (c = 0; c < ( n - 1 ); c++) {
+		      for (d = 0; d < n - c - 1; d++) {
+		        if (array[d] > array[d+1]){
+		          swap       = array[d];
+		          array[d]   = array[d+1];
+		          array[d+1] = swap;
+		        }
+		      }
+		    }
+		 
+		    System.out.println("Sorted list of your given numbers:");
+		 
+		    for (c = 0; c < n; c++)
+		      System.out.println(array[c]);
+		  
+	}
+
+}
 
 
 
