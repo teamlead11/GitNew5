e8c28a06a6dc2d1dc645d6caa2ec1033e5c10516
IbraheemMd <ibraheem.mech09@gmail.com>
2019-05-12 07:47:11 +0530

    commit

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -5,6 +5,16 @@ public class BaseClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is my base class");
+		
+		
 	}
-
+public void bubbleSortIbraheem() {
+	
+	String s= "Shahid";
+	int length= s.length();
+	
+	System.out.println(length);
+	
+	}
+	
 }
