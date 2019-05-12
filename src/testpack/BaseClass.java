0ba2a3c81560051b488e68918754fe15b29d9ece
indujaudhayak <indujaudhayak@gmail.com>
2019-05-11 23:54:39 -0400

    induja string

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -1,11 +1,21 @@
 package testpack;
 
 public class BaseClass {
+	private void reveseString() {
+	String s="induja";
+	String r ="";
+	for(int i=s.length()-1;i>=0;i++) {
+		char charAt = s.charAt(i);
+		r=r+charAt;
+	}
+System.out.println(r);
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is my base class");
 		System.out.println("Base class");
+		System.out.println("git hub");
 	}
 
 }
--- src/testpack/Induja.java
+++ src/testpack/Induja.java
@@ -1,24 +1,28 @@
 package testpack;
 
-import java.util.Scanner;
+
 
 public class Induja {
-public static void main(String[] args) {	
-		Scanner s1=new Scanner(System.in);
-		String s=s1.next();
-				String r="";
-		for (int i=s.length()-1;i>=0;i--) {
-			char ch=s.charAt(i);
-			r=r+ch;
-			
-		}
-		System.out.println(r);
-		if(r.equals(s)) {
-			System.out.println("palinrome String"); 
-		}
-		else {
-			System.out.println("not a polindrome String");
-		}
+	public static void main(String[] args) {
+		/*
+		 * Scanner s1=new Scanner(System.in); String s=s1.next(); String r=""; for (int
+		 * i=s.length()-1;i>=0;i--) { char ch=s.charAt(i); r=r+ch;
+		 * 
+		 * } System.out.println(r); if(r.equals(s)) {
+		 * System.out.println("palinrome String"); } else {
+		 * System.out.println("not a polindrome String"); }
+		 */
+		
+		
+		  String s="induja "; String res=""; 
+		  for (int i = s.length()-1; i >=0; i--) {
+		  res=res+s.charAt(i); 
+		 
+		  } System.out.println(res);
+		  
+		
+	
+
 	}
 
 
