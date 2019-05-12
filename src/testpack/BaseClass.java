8fac08849bfc61797539be7dbc3366538ec5b554
deevika sree <nireshbe1992@gmail.com1188 git config --global user.email nireshbe1992@gmail.com>
2019-05-12 14:02:15 +0530

    buble sort program added

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -5,6 +5,19 @@ public class BaseClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is my base class");
-	}
+	
+	String n="NIRESH";
+	int length= n.length();
+	System.out.println(length);
 
+	int size=n.indexOf(n);
+	System.out.println(n);
+
+}
+	public static void bubbleSortNiresh()  {
+		String n="NIRESH";
+		int length=n.length();
+		System.out.println(length);
+		
+	}
 }
