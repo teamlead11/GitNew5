5dd8d2ab25dd6bbc1dbf1d6172f83abd63b8da96
subashmaveera <subashm11660@gmail.com>
2019-05-15 11:09:13 +0530

    bubblesort vanarajan

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -425,3 +425,43 @@ public void bubbleSortByDeepika(){
 		
 
 	}
+	public void bubbleSort_vanarajan()
+	{
+		
+       Scanner sc=new Scanner(System.in);
+		
+		System.out.println("enter the array length");
+		
+		int len=sc.nextInt();
+		
+		int a[]=new int[len];
+		
+		for(int i=0;i<len;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		
+		
+		for(int i=0;i<len;i++)
+		{
+			for(int j=i+1;j<len;j++)
+			{
+				if(a[i]>a[j])
+				{
+					int temp=a[i];
+					a[i]=a[j];
+					a[j]=temp;
+					
+				}
+			}
+		}
+					
+	
+		System.out.println("ascending order");		
+		 for(int i=0;i<len;i++)
+		 {
+			 System.out.println(a[i]);
+		 }
+		
+
+	}
