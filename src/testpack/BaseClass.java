b79b008f1bab726f26393c8ef0d4d2fc057438f9
manier404 <t.manier404@gmail.com>
2019-05-15 20:51:24 +0530

    added bubble sort

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -2,71 +2,36 @@ package testpack;
 
 public class BaseClass {
 
-	 static void bubbleSortbyManivannan(int[] arr) {
-	      int n = arr.length;
-	      int temp = 0;
-
-
-	      for(int i = 0; i < n; i++) {
-	         for(int j=1; j < (n-i); j++) {
-	            if(arr[j-1] > arr[j]) {
-	               temp = arr[j-1];
-	               arr[j-1] = arr[j];
-	               arr[j] = temp;
-	            }
-	         }
-	      }
-	   }
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("this is my base class");
-	
-
-
-
-	
-	int a[]= {10,2,6,29};
-	
-	int temp=0;
+	 void bubbleSort(int arr[])
+	    {
+	        int n = arr.length;
+	        for (int i = 0; i < n-1; i++)
+	            for (int j = 0; j < n-i-1; j++)
+	                if (arr[j] > arr[j+1])
+	                {
+	                    // swap temp and arr[i]
+	                    int temp = arr[j];
+	                    arr[j] = arr[j+1];
+	                    arr[j+1] = temp;
+	                }
+	    }
 	 
-    int len=a.length;
-    
-	
-	for (int i=0;i<len;i++)
-		{
-		for (int j=1;j<(len-i);j++)
-		{
-			if (a[j-1]>a[j])
-			{
-				temp=a[j-1];
-				a[j-1]=a[j];
-				a[j]=temp;
-	
-			}
-		}
-		}
-	
-
-		for (int i=0;i<len;i++)
-		{
-			System.out.println(a[i]);
-		}
-
-		System.out.println("this is Kalaiselvan base class");
-			
-	
+	    /* Prints the array */
+	    void printArray(int arr[])
+	    {
+	        int n = arr.length;
+	        for (int i=0; i<n; ++i)
+	            System.out.print(arr[i] + " ");
+	        System.out.println();
+	    }
+	 
+	    // Driver method to test above
+	    public static void main(String args[])
+	    {
+	    	BaseClass ob = new BaseClass();
+	        int arr[] = {64, 34, 25, 12, 22, 11, 90};
+	        ob.bubbleSort(arr);
+	        System.out.println("Sorted by - Manivannan");
+	        ob.printArray(arr);
+	    }
 }
-
-
-
-private void method() {
-	// TODO Auto-generated method stub
-System.out.println("Resoving Confict by Manivannan");
-}
-}
-
-
-
-
-
